cc0b196108f0be62ac5f39d56eec5a843a699e9d
Sayonika Das <dassayonika@gmail.com>
2020-12-06 21:01:08 +0530

    link underlines removed

--- client/app/components/Header/Header.js
+++ client/app/components/Header/Header.js
@@ -3,17 +3,17 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Header = () => (
-  <header className="header" style={{backgroundColor:"tomato",textAlign:'left'}}>
+  <header className="header" style={{backgroundColor:"#ff726f",textAlign:'left'}}>
     <h3>&nbsp;</h3>
     <div style={{textAlign:"right"}}> 
     <button type="button" style={{height:"50px",width:"100px",backgroundColor:"darkorange",borderRadius:"12.5px"}}>
-      <Link to="/" style={{fontSize:"1.2em"}}><b>Home</b></Link></button>&emsp;&emsp;
+      <Link to="/" style={{fontSize:"1.2em", textDecoration:"none"}}><b>Home</b></Link></button>&emsp;&emsp;
       <button type="button" style={{height:"50px",width:"100px",backgroundColor:"darkorange",borderRadius:"12.5px"}}>
-    <Link to="/about" style={{fontSize:"1.2em"}}><b>About</b></Link></button>&emsp;&emsp;
+    <Link to="/about" style={{fontSize:"1.2em", textDecoration:"none"}}><b>About</b></Link></button>&emsp;&emsp;
     <button type="button" style={{height:"50px",width:"100px",backgroundColor:"darkorange",borderRadius:"12.5px"}}>
-    <Link to="/FAQs" style={{fontSize:"1.2em"}}><b>FAQs</b></Link></button>&emsp;&emsp;
+    <Link to="/FAQs" style={{fontSize:"1.2em", textDecoration:"none"}}><b>FAQs</b></Link></button>&emsp;&emsp;
     <button type="button" style={{height:"50px",width:"105px",backgroundColor:"darkorange",borderRadius:"12.5px"}}>
-    <Link to="/Franchise" style={{fontSize:"1.2em"}}><b>Franchise</b></Link></button>&emsp;&emsp; 
+    <Link to="/Franchise" style={{fontSize:"1.2em", textDecoration:"none"}}><b>Franchise</b></Link></button>&emsp;&emsp; 
     </div>
 
     <h1 style={{font:"Gill Sans, sans-serif"}, {textAlign:"center"}}>VEGAN PIZZA PRESS</h1>
